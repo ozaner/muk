d43fede44176a3adc81cf81238a5d364a0b0f243
ozaner <56015962+ozaner@users.noreply.github.com>
2024-04-26 09:27:08 -0400

    Add -s and -d flags to message command
    For summary and description respectively.

--- src/message.rs
+++ src/message.rs
@@ -11,14 +11,37 @@ enum Subcommand {
 pub struct Message {
     cmd: Subcommand,
 
+    /// Operate on only the message summary
+    #[arg(short, long)]
+    summary: bool,
+
+    /// Operate on only the message description
+    #[arg(short, long)]
+    description: bool,
+
     #[arg(default_value = "head")]
     hash: Hash,
 }
 
 impl MukSubcommand for Message {
     fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
+        if self.summary && self.description {
+            return Err("Cannot specify both --summary (-s) and --description (-d)".into());
+        }
         match self.cmd {
-            Subcommand::Get => Ok(println!("{}", util::git_show_format("%B", &self.hash)?)),
+            Subcommand::Get => Ok(println!(
+                "{}",
+                util::git_show_format(
+                    if self.summary {
+                        "%s"
+                    } else if self.description {
+                        "%b"
+                    } else {
+                        "%B"
+                    },
+                    &self.hash
+                )?
+            )),
         }
     }
 }
